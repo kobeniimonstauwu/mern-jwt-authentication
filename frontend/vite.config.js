8a9f68b9cb499de2a4c83601062a025094a77c29
Aelius <acemadamba16@gmail.com>
2025-01-23 02:12:53 +0800

    last test

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [
+        /^node:.*/,
+      ]
+    }
+  },
   server: {
     port: 3000,
     proxy: {
