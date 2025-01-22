53984cce746bc8ddfa03cf4e327a87575d19a9d2
Aelius <acemadamba16@gmail.com>
2025-01-23 02:10:46 +0800

    test 3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { builtinModules } from 'module';
+
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,14 +10,9 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true
-      }
+          target: 'http://localhost:5000',
+          changeOrigin: true
     }
-  },
-  build: {
-    rollupOptions: {
-      external: ['fsevents', ...builtinModules, ...builtinModules.map((m) => `node:${m}`)]
     }
   }
 })
