3425b2be59642b26f905eac52554d78d66fdec07
Aelius <acemadamba16@gmail.com>
2025-01-23 02:00:12 +0800

    test2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import { builtinModules } from 'module';
+
+const allExternal = [
+    ...builtinModules,
+    ...builtinModules.map((m) => `node:${m}`)
+]
+
+// add the following to your config object
+
+return {
+    build: {
+            rollupOptions: {
+                external: ['fsevents', ...allExternal]
+            }
+    }
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    }
-  },
   server: {
     port: 3000,
     proxy: {
