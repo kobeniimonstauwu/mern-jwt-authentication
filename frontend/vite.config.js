0c48c38fb34e954a8586766471daa9c736127cfd
Aelius <acemadamba16@gmail.com>
2025-01-23 02:02:06 +0800

    testo

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,22 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 import { builtinModules } from 'module';
-
-const allExternal = [
-    ...builtinModules,
-    ...builtinModules.map((m) => `node:${m}`)
-]
-
-// add the following to your config object
-
-return {
-    build: {
-            rollupOptions: {
-                external: ['fsevents', ...allExternal]
-            }
-    }
-}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -25,9 +9,14 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-          target: 'http://localhost:5000',
-          changeOrigin: true
+        target: 'http://localhost:5000',
+        changeOrigin: true
+      }
     }
+  },
+  build: {
+    rollupOptions: {
+      external: ['fsevents', ...builtinModules, ...builtinModules.map((m) => `node:${m}`)]
     }
   }
 })
